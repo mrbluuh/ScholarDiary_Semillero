fbf1e1b0413a2438d3874b57d7b9f461c84ce0f3
MrBluuH <salazarvl46@gmail.com>
2018-02-20 18:27:09 -0500

    Init

---
+++ ScholarDiary/src/App.js
@@ -0,0 +1,16 @@
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+class App extends Component{
+  render() {
+    return (
+    );
+  }
+}
+
+export default App;
